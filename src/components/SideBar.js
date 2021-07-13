f39cdd1aa5c07ef0876ad18a6c09186e0f432088
Sarita <dsarritta@gmail.com>
2021-07-13 19:54:58 +0545

    sidebar: design sidebar

---
+++ src/components/SideBar.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import dashboard from "../templates/sidebar/dashboard.svg";
+import leave1 from "../templates/sidebar/leave-request.svg";
+import leave2 from "../templates/sidebar/leave-details.svg";
+import leave3 from "../templates/sidebar/leave-summary.svg";
+
+export default class SideBar extends Component {
+  render() {
+    return (
+      <aside className="sidenav">
+        <div className="sidenav__title">
+          <h3 className="brand">
+            <span>fuse | HRM</span>
+          </h3>
+        </div>
+
+        <ul className="sidenav__list">
+          <li className="sidenav__list__item">
+            <a href="">
+              <span className="sidenav__list__item--icon">
+                <img src={dashboard} />
+              </span>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li className="sidenav__list__item">
+            <a href="">
+              <span className="sidenav__list__item--icon">
+                <img src={leave1} />
+              </span>
+              <span>Leave Request</span>
+            </a>
+          </li>
+          <li className="sidenav__list__item">
+            <a href="">
+              <span className="sidenav__list__item--icon">
+                <img src={leave2} />
+              </span>
+              <span>Leave Details</span>
+            </a>
+          </li>
+          <li className="sidenav__list__item">
+            <a href="">
+              <span className="sidenav__list__item--icon">
+                <img src={leave3} />
+              </span>
+              <span>Leave Summary</span>
+            </a>
+          </li>
+        </ul>
+      </aside>
+    );
+  }
+}
