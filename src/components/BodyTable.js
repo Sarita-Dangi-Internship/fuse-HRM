880ecd94536f489069c31cec7aeed4a14c42a47b
jabuna07 <jabuna007@gmail.com>
2021-07-14 18:52:54 +0545

    design v-1.1

--- src/components/BodyTable.js
+++ src/components/BodyTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { withStyles, makeStyles, } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import {
   Table,
   TableBody,
@@ -15,49 +15,44 @@ import {
   TableFooter,
 } from "@material-ui/core";
 
-
 const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 1006,
+  },
+  tableHeaderCell: {
+    fontSize: 14,
+    backgroundColor: "white",
+    color: "#8B849A",
+  },
+  avatar: {
+    backgroundColor: "red",
+    color: "black",
+  },
 
-    tableContainer: {
-        borderRadius: 15,
-        margin: '10px 10px',
-        maxWidth: 950
-    },
-    tableHeaderCell: {
-        fontWeight: 'bold',
-        backgroundColor: theme.palette.primary.dark,
-        color: theme.palette.getContrastText(theme.palette.primary.dark)
-    },
-    avatar: {
-        backgroundColor: theme.palette.primary.light,
-        color: theme.palette.getContrastText(theme.palette.primary.light)
-    },
-    name: {
-        fontWeight: 'bold',
-        color: theme.palette.secondary.dark
-    },
-    status: {
-        fontWeight: 'bold',
-        fontSize: '0.75rem',
-        color: 'white',
-        backgroundColor: 'grey',
-        borderRadius: 8,
-        padding: '3px 10px',
-        display: 'inline-block'
-    }
-  }));
+  status: {
+    fontSize: 14,
+    color: "white",
+    borderRadius: 6,
+    padding: "3px 10px",
+  },
+  avatar: {
+    height: "25px",
+    width: "25px",
+  },
+}));
 
 const StyledTableCell = withStyles((theme) => ({
   head: {},
   body: {
     fontSize: 14,
+    color: "#8B849A",
   },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
+      backgroundColor: "#E5EAF1",
     },
     body: {
       fontSize: 14,
@@ -65,14 +60,12 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
-
-
 let USERS = [],
   LEAVE = ["Annual Leave", "Sick Leave"],
   REASON = ["Personal", "Headache"],
   STATUS = ["Pending", "Approved", "Rejected"];
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 14; i++) {
   USERS[i] = {
     leaveName: LEAVE[Math.floor(Math.random() * LEAVE.length)],
     from: "5/6/2021",
@@ -87,27 +80,59 @@ for (let i = 0; i < 20; i++) {
 }
 
 export default function BodyTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   const classes = useStyles();
   console.log(USERS);
   return (
-    <div className="detailBody">
+    <div className="detailBody" style={{display:'flex'}}>
       <TableContainer component={Paper}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <StyledTableCell className={classes.tableHeaderCell}>Leave&nbsp;Name</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>From</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>To</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Leave&nbsp;Type</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Days</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Verified&nbsp;By</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Status</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Reason</StyledTableCell>
-              <StyledTableCell className={classes.tableHeaderCell}>Actions</StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Leave&nbsp;Name
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                From
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                To
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Leave&nbsp;Type
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Days
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Verified&nbsp;By
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Status
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Reason
+              </StyledTableCell>
+              <StyledTableCell className={classes.tableHeaderCell}>
+                Actions
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {USERS.map((row) => (
+            {USERS.slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage
+            ).map((row) => (
               <StyledTableRow key={row.name}>
                 <StyledTableCell component="th" scope="row">
                   {row.leaveName}
@@ -118,22 +143,47 @@ export default function BodyTable() {
                 <StyledTableCell>{row.days}</StyledTableCell>
                 <StyledTableCell>{row.verifiedBy}</StyledTableCell>
                 <StyledTableCell>
-                <Typography 
+                  <Typography
                     className={classes.status}
                     style={{
-                        backgroundColor: 
-                        ((row.status === 'Approved' && 'green') ||
-                        (row.status === 'Pending' && 'blue') ||
-                        (row.status === 'Rejected' && 'red'))
+                      backgroundColor:
+                        (row.status === "Approved" && "#5A9E00") ||
+                        (row.status === "Pending" && "#2882E8") ||
+                        (row.status === "Rejected" && "#C1451E"),
+                      textAlign: "center",
                     }}
                   >
-                    {row.status} </Typography>
-                    </StyledTableCell>
+                    {row.status}{" "}
+                  </Typography>
+                </StyledTableCell>
                 <StyledTableCell>{row.reason}</StyledTableCell>
-                <StyledTableCell>{row.actions}</StyledTableCell>
+                <StyledTableCell style={{position:'relative',}}>
+                  
+                    <Avatar
+                      src="edit.svg"
+                      className={classes.avatar}
+                      style={{ position: 'absolute', top: '15px', left: '0px' }}
+                    />
+                    <Avatar
+                      src="remove.svg"
+                      className={classes.avatar}
+                      style={{ position: 'absolute', top: '15px', left: '40px' }}
+                    />
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
+          <TableFooter>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 15]}
+              component="div"
+              count={USERS.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </TableFooter>
         </Table>
       </TableContainer>
     </div>
