4b6fd59e7eabafec392907fd22f6fa72d34ad3b9
Akrit08 <newar.akrit08@gmail.com>
2021-07-15 20:01:17 -0300

    change the component in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
-<<<<<<< HEAD
-import LeaveSummary from "./component/LeaveSummary";
+import Holiday from "./components/holiday";
 import "bootstrap/dist/css/bootstrap.min.css";
-=======
-// import Holiday from "./components/holiday";
-import "bootstrap/dist/css/bootstrap.min.css";
-import LeaveSummary from "./components/leaveSummary";
->>>>>>> 171cf4f87423fc85b7abd8e7a32892ad237e82eb
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
-      {/* <Holiday /> */}
->>>>>>> 171cf4f87423fc85b7abd8e7a32892ad237e82eb
-      <LeaveSummary />
+      <Holiday />
     </div>
   );
 }
