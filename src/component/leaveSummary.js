8b73b18759cf0416fc0c2e786662ebeb6fe8412b
Akrit08 <newar.akrit08@gmail.com>
2021-07-15 15:58:09 -0300

    display of Leave Summary not using css of Figma. Remarks: better and simple.

--- src/component/leaveSummary.js
+++ src/component/leaveSummary.js
@@ -63,86 +63,51 @@ export class LeaveSummary extends Component {
           bal: 2.5,
         },
       ],
-      pageNum: 1,
     };
   }
   render() {
     return (
       <div>
-        {/* Body */}
-        <div className="top_page body">
-          <p className="font_body top_page inner_title">Leave Summary</p>
-          {/* Table */}
+        <div className="header">
+          <p className="title font_style">Leave Summary</p>
+        </div>
+        <div className="body">
           <Table striped borderless>
-            {TableHead()}
-            {this.state.users.map((user, i) => {
-              // i % 4 ? this.setState(this.state.users.pageNum, 1) : "";
-              return (
-                <tbody className="font_body" key={i}>
-                  {UserData(user)}
-                </tbody>
-              );
-            })}
+            <thead>
+              <tr className="font_style">
+                <th>Name</th>
+                <th>Opening Balance</th>
+                <th>Allocated Date</th>
+                <th>Accumulation Days</th>
+                <th>Accumulation Hours</th>
+                <th>Adjusted</th>
+                <th>Total</th>
+                <th>Used</th>
+                <th>Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user) => {
+                return (
+                  <tr className="font_style">
+                    <td>{user.name}</td>
+                    <td>{user.opening_bal.toFixed(1)}</td>
+                    <td>{user.allocated_date.toFixed(1)}</td>
+                    <td>{user.accumulation_days.toFixed(1)}</td>
+                    <td>{user.accumulation_hrs.toFixed(1)}</td>
+                    <td>{user.adjusted.toFixed(1)}</td>
+                    <td>{user.total.toFixed(1)}</td>
+                    <td>{user.used.toFixed(1)}</td>
+                    <td>{user.bal.toFixed(1)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </Table>
-          {/* Page Number */}
-          {Footer(this.state)}
         </div>
       </div>
     );
   }
 }
 
-const UserData = (user) => {
-  return (
-    <tr>
-      <td>{user.name}</td>
-      <td>{user.opening_bal.toFixed(1)}</td>
-      <td>{user.allocated_date.toFixed(1)}</td>
-      <td>{user.accumulation_days.toFixed(1)}</td>
-      <td>{user.accumulation_hrs.toFixed(1)}</td>
-      <td>{user.adjusted.toFixed(1)}</td>
-      <td>{user.total.toFixed(1)}</td>
-      <td>{user.used.toFixed(1)}</td>
-      <td>{user.bal.toFixed(1)}</td>
-    </tr>
-  );
-};
-
-const TableHead = () => {
-  return (
-    <thead className="font_body">
-      <tr>
-        <th>Name</th>
-        <th>Opening Balance</th>
-        <th>Allocated Date</th>
-        <th>Accumulation Days</th>
-        <th>Accumulation Hours</th>
-        <th>Adjusted</th>
-        <th>Total</th>
-        <th>Used</th>
-        <th>Balance</th>
-      </tr>
-    </thead>
-  );
-};
-
-const Footer = (state) => {
-  return (
-    <div>
-      <a href="/" className="btn_props previous round">
-        &#8249;
-      </a>
-      <a href="/" className="btn_props next round">
-        &#8250;
-      </a>
-      <a href="/" className="btn_props prev_num curved_side">
-        {state.pageNum}
-      </a>
-      <p className="txt">of</p>
-      <a href="/" className="btn_props next_num curved_side">
-        {state.pageNum + 1}
-      </a>
-    </div>
-  );
-};
 export default LeaveSummary;
