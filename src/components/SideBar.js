836cb97f6d4de559dda45cdf75ac8e1af766604f
Sarita <dsarritta@gmail.com>
2021-07-16 13:56:11 +0545

    sidebar design: fix sidebar design on hover

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import dashboard from "../templates/sidebar/dashboard.svg";
-import leave1 from "../templates/sidebar/leave-request.svg";
-import leave2 from "../templates/sidebar/leave-details.svg";
-import leave3 from "../templates/sidebar/leave-summary.svg";
+// import dashboard from "../templates/sidebar/dashboard.svg";
+// import leave1 from "../templates/sidebar/leave-request.svg";
+// import leave2 from "../templates/sidebar/leave-details.svg";
+// import leave3 from "../templates/sidebar/leave-summary.svg";
 
 export default class SideBar extends Component {
   render() {
@@ -18,32 +18,32 @@ export default class SideBar extends Component {
         <ul className="sidenav__list">
           <li className="sidenav__list__item">
             <a href="">
-              <span className="sidenav__list__item--icon">
-                <img src={dashboard} />
+              <span className="sidenav__list__item--icon dashboard-icon">
+                {/* <img src={dashboard} /> */}
               </span>
               <span>Dashboard</span>
             </a>
           </li>
           <li className="sidenav__list__item">
             <a href="">
-              <span className="sidenav__list__item--icon">
-                <img src={leave1} />
+              <span className="sidenav__list__item--icon leave-request-icon">
+                {/* <img src={leave1} /> */}
               </span>
               <span>Leave Request</span>
             </a>
           </li>
           <li className="sidenav__list__item">
             <a href="">
-              <span className="sidenav__list__item--icon">
-                <img src={leave2} />
+              <span className="sidenav__list__item--icon leave-details-icon">
+                {/* <img src={leave2} /> */}
               </span>
               <span>Leave Details</span>
             </a>
           </li>
           <li className="sidenav__list__item">
             <a href="">
-              <span className="sidenav__list__item--icon">
-                <img src={leave3} />
+              <span className="sidenav__list__item--icon leave-summary-icon">
+                {/* <img src={leave3} /> */}
               </span>
               <span>Leave Summary</span>
             </a>
